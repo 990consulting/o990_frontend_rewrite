ff9b5750b925ee3ebebf024187716b31e5dbd247
David Bruce Borenstein <dborenst@gmail.com>
2018-12-30 19:24:26 -0500

    Gave up on fixing CSS/themes for now. Using Ross' themes, and will create an UpWork job to improve themes further once we've got this sucker live.

--- src/orgProfile/DummyExpansionPanel.js
+++ src/orgProfile/DummyExpansionPanel.js
@@ -5,15 +5,101 @@ import ExpansionPanelDetails from '@material-ui/core/ExpansionPanelDetails';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import Typography from '@material-ui/core/Typography';
 import Grid from '@material-ui/core/Grid';
+import withStyles from '@material-ui/core/styles/withStyles';
+import classNames from 'classnames';
+
+const styles = (theme) => ({
+  root: {},
+  table: {
+    overflowX: 'auto',
+    display: 'block'
+  },
+  expansionDetails: {
+    display: 'flex',
+    flexDirection: 'column',
+    padding: '8px 0 24px 24px'
+  },
+  expanded: {
+    margin: 0,
+    background: `${theme.color.white} !important`
+  },
+  summaryRoot: {
+    '&:hover': {
+      background: theme.color.background.desaturated
+    }
+  },
+  summaryIcon: {
+    color: theme.color.white
+  },
+  summary: {
+    background: theme.color.primary.desaturated,
+    '&:hover': {
+      backgroundColor: `${theme.color.primary.desaturated} !important`
+    }
+  },
+  summaryContent: {
+    textAlign: 'left',
+    fontFamily: theme.typography.fontFamily.heading,
+    backgroundColor: theme.color.primary.desaturated,
+    fontSize: '1.125rem',
+    fontStyle: 'normal',
+    '&>p': {
+      fontWeight: 'bold',
+      color: `${theme.color.white} !important`
+    }
+  },
+  content: {
+    textAlign: 'left'
+  },
+  structureWrapper: {
+    boxShadow: '0 2px 4px 0 #e3e3e3',
+  },
+  tableRow: {
+    border: `1px solid ${theme.color.grey.standard}`,
+    '& td': {
+      borderLeft: `1px solid ${theme.color.grey.standard}`,
+    }
+  },
+  periodsTableRow: {
+    backgroundColor: theme.color.background.desaturated,
+    border: `1px solid ${theme.color.grey.standard}`,
+    '& td': {
+      textAlign: 'center',
+      borderLeft: `1px solid ${theme.color.grey.standard}`,
+    }
+  },
+  tableCell: {
+    textAlign: 'center',
+    width: '6rem',
+    [theme.breakpoints.up('sm')]: {
+      wordBreak: 'break-word'
+    }
+  },
+  tableCellName: {
+    textAlign: 'left',
+    width: 'auto'
+  },
+  tbody: {
+    display: 'inline-table',
+    width: '98%'
+  }
+});
 
 class DummyExpansionPanel extends React.Component {
   render() {
+    const { classes } = this.props;
     return(<Grid item xs={12}>
-      <ExpansionPanel>
-        <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
-          <Typography>Expansion Panel 1</Typography>
+      <ExpansionPanel classes={{ expanded: classes.expanded}}>
+        <ExpansionPanelSummary
+          classes={{
+            root: classes.summary,
+            content: classes.summaryContent
+          }}
+          expandIcon={<ExpandMoreIcon />}
+        >
+          <Typography className={classes.heading}>Example panel</Typography>
         </ExpansionPanelSummary>
-        <ExpansionPanelDetails>
+        <ExpansionPanelDetails classes={{root: classes.expansionDetails}}>
           <Typography>
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex,
             sit amet blandit leo lobortis eget.
@@ -24,4 +110,4 @@ class DummyExpansionPanel extends React.Component {
   }
 }
 
-export default DummyExpansionPanel;
+export default withStyles(styles)(DummyExpansionPanel);
---
+++ src/orgProfile/cardStyles.js
@@ -0,0 +1,99 @@
+export const styles = (theme) => ({
+  paper: {
+    minWidth: '18rem',
+    maxWidth: '23.75rem',
+    position: 'relative',
+    textAlign: 'center',
+    borderRadius: 0,
+    [theme.breakpoints.down('xs')]: {
+      margin: '2rem 1rem 0 1rem',
+    },
+    [theme.breakpoints.only('sm')]: {
+      marginTop: '2.25rem',
+      minWidth: '35rem',
+    },
+    [theme.breakpoints.only('md')]: {
+      maxWidth: '18rem',
+    },
+    
+  },
+  title: {
+    minHeight: theme.spacing.unit,
+    backgroundColor: theme.color.primary.desaturated
+  },
+  titleText: {
+    fontFamily: theme.typography.fontFamily.heading,
+    backgroundColor: theme.color.primary.desaturated,
+    padding: '0.78rem 1.8rem',
+    fontSize: '1.125rem',
+    fontWeight: 'bold',
+    fontStyle: 'normal',
+    fontStretch: 'condensed',
+    lineHeight: 'normal',
+    letterSpacing: 'normal',
+    color: theme.color.white,
+    display: 'flex',
+    alignItems: 'center',
+    margin: 0
+  },
+  header: {
+    fontSize: '18px',
+    textTransform: 'uppercase'
+  },
+  link: {
+    textDecoration: 'none',
+    '& a': {
+      ...theme.open990.link,
+    },
+  },
+  paragraphLink: {
+    '& a': {
+      ...theme.open990.link,
+      textDecoration: 'none'
+    },
+    [theme.breakpoints.down('lg')]: {
+      display: 'flex',
+      flexDirection: 'column',
+    },
+    [theme.breakpoints.down('sm')]: {
+      display: 'flex',
+      flexDirection: 'row',
+    },
+  },
+  button: {
+    width: '70%',
+    height: '52px',
+    marginBottom: '1vh',
+    borderColor: theme.color.primary.desaturated,
+    '& span': {
+      textTransform: 'Capitalize',
+      color: theme.color.primary.desaturated,
+      fontSize: '1.125rem',
+      fontWeight: 'bold'
+    }
+  },
+  // This classNames used for extending default classes declared above from outside components
+  extendedPaper: {},
+  extendedRibbonCard: {},
+  extendedCardContent: {},
+  extendedTitle: {},
+  extendedHeader: {},
+  extendedText: {},
+  extendedLink: {},
+  extendedButton: {},
+  
+  // Added by DBB
+  expansionPanel: {
+    marginTop: '1.5rem'
+  },
+  
+  expansionPanelSummary: {
+    minHeight: 60,
+    maxHeight: 60,
+    '&$expanded': {
+      marginTop: '1.5rem',
+      minHeight: 60,
+      maxHeight: 60,
+    },
+  }
+});
