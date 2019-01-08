171b2c9563d2e62bdbc228622fae792decee715d
David Bruce Borenstein <dborenst@gmail.com>
2019-01-07 22:06:33 -0500

    Removed sidebar from search results (temporarily).

--- src/searchResults/SearchResults.js
+++ src/searchResults/SearchResults.js
@@ -3,7 +3,7 @@
  */
 
 import React, { Fragment} from 'react';
-import SidebarPage from 'sidebarPage/SidebarPage';
+//import SidebarPage from 'sidebarPage/SidebarPage';
 import SearchResultsBody from 'searchResults/SearchResultsBody';
 import { withRouter } from 'react-router-dom';
 import withStyles from '@material-ui/core/styles/withStyles';
@@ -12,21 +12,22 @@ import { styles } from 'searchResults/searchStyles';
 class SearchResults extends React.Component {
   render() {
     const { columns, fetchResults, renameRow, handleClick } = this.props;
-    const sidebarContent = (<Fragment>
+    /*const sidebarContent = (<Fragment>
       <h2>Search parameters go here</h2>
       <h3>That wasn't so hard</h3>
       <h4>Once the code was written sanely</h4>
-    </Fragment>);
+    </Fragment>);*/
     const bodyContent = <SearchResultsBody
       columns={columns}
       fetchResults={fetchResults}
       renameRow={renameRow}
       handleClick={handleClick}
     />;
-    return (<SidebarPage
+    return bodyContent;
+    /*return (<SidebarPage
       sidebarContent={sidebarContent}
       bodyContent={bodyContent}
-    />);
+    />);*/
   }
 }
 
